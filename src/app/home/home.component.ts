44243da28c5a512e090efa8799f0a7051ee6cc53
LuisNocedal-inbest <105751084+LuisNocedal-inbest@users.noreply.github.com>
2023-05-17 16:10:22 -0600

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,17 +23,6 @@ export class HomeComponent {
   constructor(private http: HttpClient) { this.getItems() }
 
   getItems () {
-    fetch('https://m4q9bg6tyc.execute-api.us-east-2.amazonaws.com/dev/post', {
-      method: 'POST',
-      body: JSON.stringify({
-        "title": "Test",
-        "description": "Test",
-        "completed": false
-      })
-    }).then(async (response) => {
-      const result = await response.json();
-      console.log(result);
-    })
   }
 
   createItem() {
